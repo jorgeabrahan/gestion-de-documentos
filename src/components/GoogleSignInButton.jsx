f908d5905a279087bd24f61448425c57ce664bda
jorgeabrahan <jorge24abrahan@gmail.com>
2023-12-12 16:09:52 -0600

    Adaptar los componentes de botones para permitir desabilitarlos en base a una condicional

--- src/components/GoogleSignInButton.jsx
+++ src/components/GoogleSignInButton.jsx
@@ -1,15 +1,29 @@
 import { GoogleIsoLogo } from '../assets/images'
 import PropTypes from 'prop-types'
 
-export const GoogleSignInButton = ({ handleClick = () => {} }) => {
+export const GoogleSignInButton = ({
+  handleClick = () => {},
+  isDisabled = false
+}) => {
   return (
-    <button className="bg-anti-flash-white rounded-md font-semibold py-3 px-[22px] flex items-center justify-center gap-3 text-black" onClick={handleClick}>
-      <img className='max-w-[20px]' src={GoogleIsoLogo} alt="Isologo de Google" />
+    <button
+      className={`bg-anti-flash-white rounded-md font-semibold py-3 px-[22px] flex items-center justify-center gap-3 text-black ${
+        isDisabled && 'opacity-40'
+      }`}
+      onClick={handleClick}
+      disabled={isDisabled}
+    >
+      <img
+        className="max-w-[20px]"
+        src={GoogleIsoLogo}
+        alt="Isologo de Google"
+      />
       <span>Registrarse con Google</span>
     </button>
   )
 }
 
 GoogleSignInButton.propTypes = {
-    handleClick: PropTypes.func
+  handleClick: PropTypes.func,
+  isDisabled: PropTypes.bool
 }
--- src/components/PrimaryButton.jsx
+++ src/components/PrimaryButton.jsx
@@ -4,12 +4,14 @@ export const PrimaryButton = ({
   text = '',
   handleClick = () => {},
   className = '',
+  isDisabled = false,
   children
 }) => {
   return (
     <button
-      className={`bg-fire-engine-red rounded-md font-semibold py-3 px-[22px] flex items-center justify-center gap-3 ${className}`}
+      className={`bg-fire-engine-red rounded-md font-semibold py-3 px-[22px] flex items-center justify-center gap-3 ${isDisabled && 'opacity-40'} ${className}`}
       onClick={handleClick}
+      disabled={isDisabled}
     >
       {children}
       {text}
@@ -21,5 +23,6 @@ PrimaryButton.propTypes = {
   text: PropTypes.string,
   handleClick: PropTypes.func,
   className: PropTypes.string,
+  isDisabled: PropTypes.bool,
   children: PropTypes.node
 }
