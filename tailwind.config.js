d9416cec17412a90e0a4517094d17730cd166656
jorgeabrahan <jorge24abrahan@gmail.com>
2023-12-12 15:24:33 -0600

    Agregar mas colores a la paleta de la pagina web en tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,34 @@
 export default {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'raisin-black': {
+          DEFAULT: '#262628',
+          800: '#252526'
+        },
+        onyx: {
+          DEFAULT: '#424245'
+        },
+        'anti-flash-white': {
+          DEFAULT: '#f5f5f7'
+        },
+        'eerie-black': {
+          DEFAULT: '#1d1d1f',
+          800: '#1f1f1f'
+        },
+        'dim-gray': {
+          DEFAULT: '#6E6E73'
+        },
+        'fire-engine-red': {
+          DEFAULT: '#d60017',
+          500: '#fa233b'
+        },
+        'true-blue': {
+          DEFAULT: '#0066CC'
+        }
+      }
+    },
   },
   plugins: [],
 }
