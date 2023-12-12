6c447ebe1df5691ea4f0ef5e2c741df3d3016f78
jorgeabrahan <jorge24abrahan@gmail.com>
2023-12-12 15:22:55 -0600

    Crear el estilo del formulario de inicio de sesion en la pagina de login con los componentes creados

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,13 +1,40 @@
+import {
+  Input,
+  PrimaryButton,
+  GoogleSignInButton,
+  SectionTitle,
+  SectionSubtitle
+} from '../components'
+import { PrimaryLink } from '../components/PrimaryLink'
+import { CenteredBoxLayout } from '../layouts'
+
 export const LoginPage = () => {
   return (
-    <>
-      <h1>Iniciar sesion</h1>
-      <form>
-        <label htmlFor="email">Correo:</label>
-        <input type="email" id="email" name="email" />
-        <label htmlFor="password">Contraseña:</label>
-        <input type="password" name="password" id="password" />
-      </form>
-    </>
+    <main className="delimiter grid place-items-center h-[100vh]">
+      <CenteredBoxLayout>
+        <SectionTitle isCentered={true} text="Inicia Sesión" />
+        <SectionSubtitle
+          isCentered={true}
+          className="mb-8"
+          text="Accede a tu cuenta para iniciar."
+        />
+        <form className="grid gap-4">
+          <Input label="Correo electronico" id="email" />
+          <Input
+            className="mb-8"
+            label="Contraseña"
+            id="password"
+            type="password"
+          />
+          <PrimaryButton text="Iniciar sesión" className="w-full" />
+          <GoogleSignInButton />
+        </form>
+        <PrimaryLink
+          className="block mx-auto mt-4"
+          text="¿No tienes una cuenta?"
+          to='/crear-cuenta'
+        />
+      </CenteredBoxLayout>
+    </main>
   )
 }
