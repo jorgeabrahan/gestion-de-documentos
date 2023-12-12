460c1a05523fbeea292427f9f88ba84e3a3ab276
jorgeabrahan <jorge24abrahan@gmail.com>
2023-12-12 15:16:26 -0600

    Crear rutas para la pagina de inicio de sesion y registro (crear cuenta)

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from "react-router-dom";
-import { HomePage, LoginPage } from "./pages";
+import { HomePage, LoginPage, SignUp } from "./pages";
 
 export const AppRouter = () => {
   return (
     <Routes>
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/iniciar-sesion" element={<LoginPage />} />
+        <Route path="/crear-cuenta" element={<SignUp />} />
         <Route path="/" element={<HomePage />} />
     </Routes>
   );
