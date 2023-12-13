5f94e1bcfc75194eb42dca6a013c15bec9bdf75d
jorgeabrahan <jorge24abrahan@gmail.com>
2023-12-12 19:27:35 -0600

    Actualizar las rutas para determinar cuales seran privadas y publicas y asi permitir el acceso o no en base a si el usuario esta autenticado o no

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,12 +1,17 @@
-import { Route, Routes } from "react-router-dom";
-import { HomePage, LoginPage, SignUp } from "./pages";
+import { Route, Routes } from 'react-router-dom'
+import { HomePage, LoginPage, SignUp } from './pages'
+import { PublicRoute } from './PublicRoute'
+import { PrivateRoute } from './PrivateRoute'
 
 export const AppRouter = () => {
   return (
     <Routes>
-        <Route path="/iniciar-sesion" element={<LoginPage />} />
-        <Route path="/crear-cuenta" element={<SignUp />} />
-        <Route path="/" element={<HomePage />} />
+      <Route
+        path="/iniciar-sesion"
+        element={<PublicRoute element={LoginPage} />}
+      />
+      <Route path="/crear-cuenta" element={<PublicRoute element={SignUp} />} />
+      <Route path="/" element={<PrivateRoute element={HomePage} />} />
     </Routes>
-  );
+  )
 }
