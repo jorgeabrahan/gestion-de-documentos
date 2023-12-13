3796c2e70c7914a6b1dfc67e9cf700be91e442a6
jorgeabrahan <jorge24abrahan@gmail.com>
2023-12-13 09:21:03 -0600

    Agregar modal, Toaster y componente de navbar en el layout principal de todas las paginas

--- src/pages/PageLayout.jsx
+++ src/pages/PageLayout.jsx
@@ -1,15 +1,33 @@
 import PropTypes from 'prop-types'
+import { Navbar } from '../global/Navbar'
+import { Toaster } from 'react-hot-toast'
+import { useEffect } from 'react'
+import { MODAL_IDS, authStore, dataStore } from '../stores'
+import { useNavigate } from 'react-router-dom'
+import { ChangePasswordModal } from '../modals'
 
-export const PageLayout = ({ children }) => {
+export const PageLayout = ({ children, needsToBeAdmin = false }) => {
+  const { user } = authStore((store) => store)
+  const { modalToShow } = dataStore(store => store)
+  const navigate = useNavigate()
+  useEffect(() => {
+    if (needsToBeAdmin && user.role !== 'admin') {
+      navigate('/', { replace: true })
+    }
+  }, [navigate, needsToBeAdmin, user.role])
   return (
     <>
-      <p>navbar</p>
-      {children}
-      <p>footer</p>
+      <Toaster position="top-center" reverseOrder={false} />
+      {modalToShow === MODAL_IDS.changePassword && (
+        <ChangePasswordModal />
+      )}
+      <Navbar />
+      <main className="delimiter">{children}</main>
     </>
-  );
+  )
 }
 
 PageLayout.propTypes = {
-  children: PropTypes.node
+  children: PropTypes.node,
+  needsToBeAdmin: PropTypes.bool
 }
