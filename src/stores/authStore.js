c8b7902bbc414e185e6a647ab43281213c421c6c
jorgeabrahan <jorge24abrahan@gmail.com>
2023-12-12 18:41:21 -0600

    Crear obtjeto con los roles permitidos, agregar la propiedad de rol al usuario por defecto y crear un metodo en el store para cambiar o establecer el role del usuario que esta actualmente autenticado

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -1,18 +1,31 @@
 import { create } from 'zustand'
 
 export const AUTH_STATUS = {
-    checking: 'checking',
-    authorized: 'authorized',
-    unauthorized: 'unauthorized'
+  checking: 'checking',
+  authorized: 'authorized',
+  unauthorized: 'unauthorized'
 }
 
-export const initialUser = { displayName: null, email: null, uid: null }
+export const USER_ROLES = {
+  pending: 'pending',
+  user: 'user',
+  admin: 'admin'
+}
+
+export const initialUser = {
+  displayName: null,
+  email: null,
+  uid: null,
+  role: null
+}
 
 export const authStore = create((set) => ({
   user: initialUser,
   status: AUTH_STATUS.unauthorized,
   error: null, // string || null
   setUser: (user) => set((state) => ({ ...state, user })),
+  setUserRole: (role) =>
+    set((state) => ({ ...state, user: { ...state.user, role } })),
   setStatus: (status) => set((state) => ({ ...state, status })),
   setError: (error) => set((state) => ({ ...state, error }))
 }))
