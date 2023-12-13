db553c13c06a4b6ba345b7e91c538d75c3fb40de
jorgeabrahan <jorge24abrahan@gmail.com>
2023-12-13 09:20:28 -0600

    Agregar la ruta privada para mostrar la pagina de usuarios

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
-import { HomePage, LoginPage, SignUp } from './pages'
+import { HomePage, LoginPage, SignUp, Users } from './pages'
 import { PublicRoute } from './PublicRoute'
 import { PrivateRoute } from './PrivateRoute'
 
@@ -12,6 +12,7 @@ export const AppRouter = () => {
       />
       <Route path="/crear-cuenta" element={<PublicRoute element={SignUp} />} />
       <Route path="/" element={<PrivateRoute element={HomePage} />} />
+      <Route path="/usuarios" element={<PrivateRoute element={Users} />} />
     </Routes>
   )
 }
