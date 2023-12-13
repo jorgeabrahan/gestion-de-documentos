23c536108c8182615e4fea62f36651b9b957f059
jorgeabrahan <jorge24abrahan@gmail.com>
2023-12-12 18:40:10 -0600

    Configurar la base de datos no relacional de firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,14 +1,16 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB6MkIQ8u2IQFo_tI3Wk0dKCMBzpX0wZs4",
-  authDomain: "gestion-de-documentos-8c4a4.firebaseapp.com",
-  projectId: "gestion-de-documentos-8c4a4",
-  storageBucket: "gestion-de-documentos-8c4a4.appspot.com",
-  messagingSenderId: "754809159697",
-  appId: "1:754809159697:web:2a164e94ddfa9eb499b1b0"
-};
+  apiKey: 'AIzaSyB6MkIQ8u2IQFo_tI3Wk0dKCMBzpX0wZs4',
+  authDomain: 'gestion-de-documentos-8c4a4.firebaseapp.com',
+  projectId: 'gestion-de-documentos-8c4a4',
+  storageBucket: 'gestion-de-documentos-8c4a4.appspot.com',
+  messagingSenderId: '754809159697',
+  appId: '1:754809159697:web:2a164e94ddfa9eb499b1b0'
+}
 
-export const FirebaseApp = initializeApp(firebaseConfig);
+export const FirebaseApp = initializeApp(firebaseConfig)
 export const FirebaseAuth = getAuth(FirebaseApp)
+export const FirebaseFirestore = getFirestore(FirebaseApp)
