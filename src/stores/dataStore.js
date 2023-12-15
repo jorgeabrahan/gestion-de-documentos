9d35d35b1b773bf2b5da893862d006135097c405
jorgeabrahan <jorge24abrahan@gmail.com>
2023-12-15 12:19:25 -0600

    Agregar arreglo de tipos de documentos al store de data y los metodos para modificarlo

--- src/stores/dataStore.js
+++ src/stores/dataStore.js
@@ -1,12 +1,29 @@
 import { create } from 'zustand'
 
 export const MODAL_IDS = {
-    changePassword: 'changePassword'
+  changePassword: 'changePassword',
+  createDocument: 'createDocument',
+  manageDocumentTypes: 'manageDocumentTypes'
+}
+
+export const DOCUMENT_STATES = {
+  solicitud: 'solicitud', // default
+  proceso: 'proceso',
+  cerrado: 'cerrado'
 }
 
 export const dataStore = create((set) => ({
   users: [],
+  documentTypes: [],
   modalToShow: null,
   setUsers: (users) => set((state) => ({ ...state, users })),
-  setModalToShow: (modalToShow) => set(state => ({ ...state, modalToShow }))
+  setModalToShow: (modalToShow) => set((state) => ({ ...state, modalToShow })),
+  setDocumentTypes: (documentTypes) =>
+    set((state) => ({ ...state, documentTypes })),
+  addDocumentType: (documentType) =>
+    set((state) => ({
+      ...state,
+      documentTypes: [...state.documentTypes, documentType]
+    })),
+  removeDocumentType: (documentId) => set(state => ({ ...state, documentTypes: state?.documentTypes?.filter(dt => dt.id !== documentId) }))
 }))
