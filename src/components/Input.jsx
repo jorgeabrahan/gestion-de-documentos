e252febaf507da2c0e1b0737cddda150502d8f54
jorgeabrahan <jorge24abrahan@gmail.com>
2023-12-15 11:46:11 -0600

    Arreglar bug para mostrar el input con el estado de enfocado en caso de que ya tenga un valor definido

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { EyeClosed, EyeOpen } from '../assets/icons'
 
 export const Input = ({
@@ -9,12 +9,17 @@ export const Input = ({
   isRequired = true,
   className = '',
   value = '',
-  handleChange = () => {}
+  isDisabled = false,
+  handleChange = () => {},
+  handleKeyDown = () => {}
 }) => {
   const [isPasswordVisible, setIsPasswordVisible] = useState(false)
-  const [isFocused, setIsFocused] = useState(false)
+  const [isFocused, setIsFocused] = useState(value.trim() !== '')
+  useEffect(() => {
+    if (value.trim() !== '' && !isFocused) setIsFocused(true)
+  }, [value])
   return (
-    <div className={`relative w-full ${className}`}>
+    <div className={`relative w-full ${className} ${isDisabled && 'opacity-40'}`}>
       <label
         className={`absolute left-4 text-dim-gray transition-[font-size,top] duration-300 ${
           isFocused ? 'top-3 text-xs' : 'top-5'
@@ -39,6 +44,8 @@ export const Input = ({
         autoComplete="off"
         value={value}
         onChange={handleChange}
+        onKeyDown={handleKeyDown}
+        disabled={isDisabled}
       />
       {type === 'password' && (
         <button onClick={() => setIsPasswordVisible(prev => !prev)} className='absolute right-4 top-[50%] -translate-y-[50%]' type='button'>
@@ -56,5 +63,7 @@ Input.propTypes = {
   isRequired: PropTypes.bool,
   className: PropTypes.string,
   value: PropTypes.string,
-  handleChange: PropTypes.func
+  isDisabled: PropTypes.bool,
+  handleChange: PropTypes.func,
+  handleKeyDown: PropTypes.func
 }
