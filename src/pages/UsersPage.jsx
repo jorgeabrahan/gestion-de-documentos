27b977e582e90a3edd46a1baa9091250e49dfb45
jorgeabrahan <jorge24abrahan@gmail.com>
2023-12-15 16:06:41 -0600

    Agregar boton para refrescar los usuarios en la pagina de usuarios

--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -1,28 +1,44 @@
 import { useEffect, useState } from 'react'
 import { authStore, dataStore } from '../stores'
 import { PageLayout } from './PageLayout'
-import { ColTitle, SearchInput, UserRow } from './users-page-c'
-import { RowTitleLayout } from './users-page-c/RowTitleLayout'
+import { SearchInput, UserRow } from './users-page-c'
 import { useForm } from '../hooks'
 import toast from 'react-hot-toast'
+import {
+  ActionButton,
+  ColTitle,
+  RowTitleLayout,
+  SectionSubtitle,
+  SectionTitle
+} from '../components'
+import { Refresh } from '../assets/icons'
+import { getAllUsers } from '../firebase/database'
 
 export const Users = () => {
   const { user: currentUser } = authStore((store) => store)
-  const { users } = dataStore((store) => store)
+  const { users, setUsers } = dataStore((store) => store)
   const [filteredUsers, setFilteredUsers] = useState([])
   const { filterBy, query, onInputChange } = useForm({
     filterBy: 'name',
     query: ''
   })
   const [isChangingUserRole, setIsChangingUserRole] = useState(false)
+  const [isRefreshing, setIsRefreshing] = useState(false)
   useEffect(() => {
     if (users.length !== 0) setFilteredUsers(users)
   }, [users])
   const handleSubmit = (e) => {
     e.preventDefault()
-    const filtered = users.filter(fUser => {
-      if (filterBy === 'name') return fUser?.displayName?.trim()?.toLowerCase()?.includes(query?.trim()?.toLowerCase())
-      return fUser?.email?.trim()?.toLowerCase()?.includes(query?.trim()?.toLowerCase())
+    const filtered = users.filter((fUser) => {
+      if (filterBy === 'name')
+        return fUser?.displayName
+          ?.trim()
+          ?.toLowerCase()
+          ?.includes(query?.trim()?.toLowerCase())
+      return fUser?.email
+        ?.trim()
+        ?.toLowerCase()
+        ?.includes(query?.trim()?.toLowerCase())
     })
     if (filtered.length === 0) {
       toast.error(`No se encontraron resultados para ${query}`)
@@ -32,11 +48,14 @@ export const Users = () => {
   }
   return (
     <PageLayout needsToBeAdmin={true}>
-      <form className="flex items-center justify-end mb-8 gap-3" onSubmit={handleSubmit}>
+      <form
+        className="flex items-center justify-end mb-3 gap-3"
+        onSubmit={handleSubmit}
+      >
         <select
           className="bg-[#ffffff0a] border border-solid border-onyx px-3 py-2 rounded-full text-sm w-max focus:outline-none"
-          id='filterBy'
-          name='filterBy'
+          id="filterBy"
+          name="filterBy"
           value={filterBy}
           onChange={onInputChange}
         >
@@ -49,6 +68,29 @@ export const Users = () => {
           handleChange={onInputChange}
         />
       </form>
+      <section className="flex items-center justify-end gap-3">
+        <ActionButton
+          text="Refrescar"
+          handleClick={() => {
+            setIsRefreshing(true)
+            toast(() => <span>Recargando documentos...</span>)
+            getAllUsers().then((res) => {
+              if (res?.error !== null) {
+                toast.error('No se pudieron cargar los usuarios')
+                return
+              }
+              setUsers(res?.users)
+            })
+          }}
+          isDisabled={isRefreshing}
+        >
+          <Refresh dimensions="15px" />
+        </ActionButton>
+      </section>
+      <div className="my-10">
+        <SectionTitle text="Usuarios de tu organización" />
+        <SectionSubtitle text="Todos los usuarios que forman parte de tu organización apareceran aquí" />
+      </div>
       <section className="bg-raisin-black-800 p-5 rounded-2xl">
         <RowTitleLayout>
           <ColTitle text="Nombre" />
