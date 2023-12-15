469bd217182e929cd24977d18fca7040610b3212
jorgeabrahan <jorge24abrahan@gmail.com>
2023-12-15 16:04:42 -0600

    Agregar la logica para mostrar los documentos que el usuario ha enviado en formato de tabla

--- src/pages/DocumentsPage.jsx
+++ src/pages/DocumentsPage.jsx
@@ -1,13 +1,25 @@
 import toast from 'react-hot-toast'
 import { AddDocument, ManageDocumentTypes } from '../assets/icons'
-import { ActionButton } from '../components'
+import {
+  ActionButton,
+  DocumentsTable,
+  SecondaryDescription,
+  SectionSubtitle,
+  SectionTitle
+} from '../components'
 import { MODAL_IDS, USER_ROLES, authStore, dataStore } from '../stores'
 import { PageLayout } from './PageLayout'
+import { useEffect, useState } from 'react'
 
 export const DocumentsPage = () => {
   const { setModalToShow } = dataStore((store) => store)
-  const { users, documentTypes } = dataStore((store) => store)
+  const { documents, users, documentTypes } = dataStore((store) => store)
   const { user } = authStore((store) => store)
+  const [documentsSentByYou, setDocumentsSentByYou] = useState([])
+  useEffect(() => {
+    const result = documents?.filter((doc) => doc?.sender?.uid === user?.uid)
+    setDocumentsSentByYou(result)
+  }, [documents, user?.uid])
   return (
     <PageLayout>
       <section className="flex items-center justify-end gap-3">
@@ -18,7 +30,7 @@ export const DocumentsPage = () => {
           }}
           isDisabled={user?.role !== USER_ROLES.admin}
         >
-          <ManageDocumentTypes />
+          <ManageDocumentTypes dimensions="15px" />
         </ActionButton>
         <ActionButton
           text="Crear documento"
@@ -29,14 +41,30 @@ export const DocumentsPage = () => {
               return
             }
             if (documentTypes?.length < 1) {
-              toast.error('No hay tipos de documentos creados por el administrador')
+              toast.error(
+                'No hay tipos de documentos creados por el administrador'
+              )
               return
             }
             setModalToShow(MODAL_IDS.createDocument)
           }}
         >
-          <AddDocument />
+          <AddDocument dimensions="15px" />
         </ActionButton>
+      </section>
+      <section>
+        <div className="my-10">
+          <SectionTitle text="Enviados por tí" />
+          <SectionSubtitle text="Todos los documentos que has enviado a compañeros de tu organización apareceran aquí" />
+        </div>
+        {documentsSentByYou?.length === 0 ? (
+          <SecondaryDescription
+            className="text-center py-40"
+            text="Crea documentos para verlos aqui"
+          />
+        ) : (
+          <DocumentsTable documents={documentsSentByYou} />
+        )}
       </section>
     </PageLayout>
   )
