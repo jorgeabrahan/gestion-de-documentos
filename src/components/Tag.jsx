e0b73f52199d07d0a02f013d53feeb7d0515fe82
jorgeabrahan <jorge24abrahan@gmail.com>
2023-12-15 15:58:45 -0600

    Agregar parametro para cambiar el color de las tags

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types'
 
-export const Tag = ({ text = '' }) => {
+export const Tag = ({ text = '', isBlue = false }) => {
   return (
-    <span className="bg-fire-engine-red-500 rounded-full font-semibold px-3 py-1 text-xs">{text}</span>
+    <span className={`${isBlue ? 'bg-celtic-blue' : 'bg-fire-engine-red-500'} rounded-full font-semibold px-3 py-1 text-xs w-max h-max`}>{text}</span>
   );
 }
 
 Tag.propTypes = {
-    text: PropTypes.string
+    text: PropTypes.string,
+    isBlue: PropTypes.bool
 }
