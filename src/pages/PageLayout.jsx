5c83007eedb73749202196c17a9e353f433a7eff
jorgeabrahan <jorge24abrahan@gmail.com>
2023-12-15 12:23:26 -0600

    Agregar validaciones y logica para mostar los modales agregados

--- src/pages/PageLayout.jsx
+++ src/pages/PageLayout.jsx
@@ -1,25 +1,44 @@
 import PropTypes from 'prop-types'
 import { Navbar } from '../global/Navbar'
-import { Toaster } from 'react-hot-toast'
+import toast, { Toaster } from 'react-hot-toast'
 import { useEffect } from 'react'
 import { MODAL_IDS, authStore, dataStore } from '../stores'
 import { useNavigate } from 'react-router-dom'
-import { ChangePasswordModal } from '../modals'
+import { ChangePasswordModal, CreateDocumentModal } from '../modals'
+import { getAllUsers, getDocumentTypes } from '../firebase/database'
+import { ManageDocumentTypesModal } from '../modals/ManageDocumentTypesModal'
 
 export const PageLayout = ({ children, needsToBeAdmin = false }) => {
   const { user } = authStore((store) => store)
-  const { modalToShow } = dataStore(store => store)
+  const { modalToShow, setUsers, setDocumentTypes } = dataStore(store => store)
   const navigate = useNavigate()
   useEffect(() => {
     if (needsToBeAdmin && user.role !== 'admin') {
       navigate('/', { replace: true })
     }
   }, [navigate, needsToBeAdmin, user.role])
+  // cargar todos los usuarios una vez el sitio cargue
+  useEffect(() => {
+    getAllUsers().then((users) => setUsers(users))
+    getDocumentTypes().then(res => {
+      if (res.error !== null) {
+        toast.error('No se pudieron cargar los tipos de documentos')
+        return
+      }
+      setDocumentTypes(res?.documentTypes)
+    })
+  }, [setUsers, setDocumentTypes])
   return (
     <>
       <Toaster position="top-center" reverseOrder={false} />
       {modalToShow === MODAL_IDS.changePassword && (
         <ChangePasswordModal />
+      )}
+      {modalToShow === MODAL_IDS.createDocument && (
+        <CreateDocumentModal />
+      )}
+      {modalToShow === MODAL_IDS.manageDocumentTypes && (
+        <ManageDocumentTypesModal />
       )}
       <Navbar />
       <main className="delimiter">{children}</main>
