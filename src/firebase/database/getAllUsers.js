ef36506627aeb79bcae57f5fc39f623d552d761b
jorgeabrahan <jorge24abrahan@gmail.com>
2023-12-15 16:01:13 -0600

    Actualizar funcion para obtener todas los usuarios utilizando un try catch en caso de que devuelva un error

--- src/firebase/database/getAllUsers.js
+++ src/firebase/database/getAllUsers.js
@@ -1,9 +1,13 @@
-import { collection, getDocs } from 'firebase/firestore';
-import { FirebaseFirestore } from '../config';
+import { collection, getDocs } from 'firebase/firestore'
+import { FirebaseFirestore } from '../config'
 
 export const getAllUsers = async () => {
-  const usersListRef = collection(FirebaseFirestore, 'usersList');
-  const userSnapshots = await getDocs(usersListRef);
-  const users = userSnapshots.docs.map(doc => doc.data());
-  return users;
-};
+  try {
+    const usersListRef = collection(FirebaseFirestore, 'usersList')
+    const userSnapshots = await getDocs(usersListRef)
+    const users = userSnapshots.docs.map((doc) => doc.data())
+    return { users, error: null }
+  } catch (error) {
+    return { users: null, error }
+  }
+}
