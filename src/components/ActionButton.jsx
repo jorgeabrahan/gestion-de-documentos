3a79375061a6a5a8c26aee6266b837c9f57363b9
jorgeabrahan <jorge24abrahan@gmail.com>
2023-12-15 11:46:49 -0600

    Hacer del action button un componente global

--- src/components/ActionButton.jsx
+++ src/components/ActionButton.jsx
@@ -1,11 +1,11 @@
 import PropTypes from 'prop-types'
 
-export const ActionButton = ({ children, text = '', isDisabled = false, handleClick = () => {} }) => {
+export const ActionButton = ({ children, text = '', isDisabled = false, handleClick = () => {}, className = '' }) => {
   return (
     <button
       className={`bg-celtic-blue px-3 py-1 rounded-full w-max flex items-center gap-2 ${
         isDisabled && 'opacity-40 cursor-not-allowed'
-      }`}
+      } ${className}`}
       disabled={isDisabled}
       onClick={handleClick}
     >
@@ -18,5 +18,6 @@ ActionButton.propTypes = {
   children: PropTypes.node,
   text: PropTypes.string,
   isDisabled: PropTypes.bool,
-  handleClick: PropTypes.func
+  handleClick: PropTypes.func,
+  className: PropTypes.string
 }
