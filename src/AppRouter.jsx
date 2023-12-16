8c6655f877afafc98df28aa01c5633e21714eecb
jorgeabrahan <jorge24abrahan@gmail.com>
2023-12-15 22:07:14 -0600

    Crear la ruta para la vista de un documento con el slug del id del documento

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
-import { DocumentsPage, HomePage, LoginPage, SignUp, Users } from './pages'
+import { DocumentPage, DocumentsPage, HomePage, LoginPage, SignUp, Users } from './pages'
 import { PublicRoute } from './PublicRoute'
 import { PrivateRoute } from './PrivateRoute'
 
@@ -13,6 +13,7 @@ export const AppRouter = () => {
       <Route path="/crear-cuenta" element={<PublicRoute element={SignUp} />} />
       <Route path="/" element={<PrivateRoute element={HomePage} />} />
       <Route path="/usuarios" element={<PrivateRoute element={Users} />} />
+      <Route path="/documentos/:documentId" element={<PrivateRoute element={DocumentPage} />} />
       <Route path="/documentos" element={<PrivateRoute element={DocumentsPage} />} />
     </Routes>
   )
