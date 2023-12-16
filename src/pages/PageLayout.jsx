a27dc3e38eaffc3f054ccb021502384dd8c20a9a
jorgeabrahan <jorge24abrahan@gmail.com>
2023-12-15 22:03:06 -0600

    Remover la logica para redirigir cuando el usuario necesita ser una administrador para acceder a la pagina, y agregar la logica para mostrar el componente que permite agregar comentarios

--- src/pages/PageLayout.jsx
+++ src/pages/PageLayout.jsx
@@ -2,21 +2,12 @@ import PropTypes from 'prop-types'
 import { Navbar } from '../global/Navbar'
 import toast, { Toaster } from 'react-hot-toast'
 import { useEffect } from 'react'
-import { MODAL_IDS, authStore, dataStore } from '../stores'
-import { useNavigate } from 'react-router-dom'
-import { ChangePasswordModal, CreateDocumentModal } from '../modals'
+import { MODAL_IDS, dataStore } from '../stores'
+import { ChangePasswordModal, CreateDocumentModal, ManageDocumentTypesModal, CreateCommentModal } from '../modals'
 import { getAllUsers, getDocumentTypes, getDocuments } from '../firebase/database'
-import { ManageDocumentTypesModal } from '../modals/ManageDocumentTypesModal'
 
-export const PageLayout = ({ children, needsToBeAdmin = false }) => {
-  const { user } = authStore((store) => store)
+export const PageLayout = ({ children }) => {
   const { modalToShow, setUsers, setDocumentTypes, setDocuments } = dataStore(store => store)
-  const navigate = useNavigate()
-  useEffect(() => {
-    if (needsToBeAdmin && user.role !== 'admin') {
-      navigate('/', { replace: true })
-    }
-  }, [navigate, needsToBeAdmin, user.role])
   // cargar todos los usuarios una vez el sitio cargue
   useEffect(() => {
     getAllUsers().then((res) => {
@@ -53,6 +44,9 @@ export const PageLayout = ({ children, needsToBeAdmin = false }) => {
       {modalToShow === MODAL_IDS.manageDocumentTypes && (
         <ManageDocumentTypesModal />
       )}
+      {modalToShow === MODAL_IDS.addComment && (
+        <CreateCommentModal />
+      )}
       <Navbar />
       <main className="delimiter">{children}</main>
     </>
@@ -60,6 +54,5 @@ export const PageLayout = ({ children, needsToBeAdmin = false }) => {
 }
 
 PageLayout.propTypes = {
-  children: PropTypes.node,
-  needsToBeAdmin: PropTypes.bool
+  children: PropTypes.node
 }
