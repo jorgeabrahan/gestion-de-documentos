5c3501ec6f3a8a281e4e2e6de2255ce6a4689010
jorgeabrahan <jorge24abrahan@gmail.com>
2023-12-15 21:53:59 -0600

    Convertir el documento que se muestra en la tabla de documentos en un link que lleve a la pagina del mismo

--- src/components/DocumentsTable.jsx
+++ src/components/DocumentsTable.jsx
@@ -1,7 +1,9 @@
+import { Link } from 'react-router-dom'
 import { ColTitle } from './ColTitle'
 import { RowTitleLayout } from './RowTitleLayout'
 import { Tag } from './Tag'
 import PropTypes from 'prop-types'
+import { DOCUMENT_STATES } from '../stores'
 
 export const DocumentsTable = ({ documents = [] }) => {
   return (
@@ -14,20 +16,27 @@ export const DocumentsTable = ({ documents = [] }) => {
         <ColTitle text="Estado" />
       </RowTitleLayout>
       <section className="grid gap-3">
-        {documents?.map((document) => (
-          <div
-            key={document?.id}
-            className="grid grid-cols-5 gap-3 py-[6px] cursor-pointer"
-          >
-            <p className="text-sm">{document?.creationTimeAndDate}</p>
-            <p className="text-sm font-semibold">
-              {document?.documentType?.label}
-            </p>
-            <p className="truncate text-sm">{document?.subject}</p>
-            <p className="line-clamp-3 text-sm">{document?.description}</p>
-            <Tag text={document?.state} />
-          </div>
-        ))}
+        {documents?.map((document) => {
+          let backgroundColor = 'bg-yellow'
+          if (document?.state === DOCUMENT_STATES.proceso)
+            backgroundColor = 'bg-celtic-blue'
+          if (document?.state === DOCUMENT_STATES.cerrado) backgroundColor = ''
+          return (
+            <Link
+              to={`/documentos/${document?.id}`}
+              key={document?.id}
+              className="grid grid-cols-5 gap-3 py-[6px] cursor-pointer"
+            >
+              <p className="text-sm">{document?.creationTimeAndDate}</p>
+              <p className="text-sm font-semibold">
+                {document?.documentType?.label}
+              </p>
+              <p className="truncate text-sm">{document?.subject}</p>
+              <p className="line-clamp-3 text-sm">{document?.description}</p>
+              <Tag text={document?.state} className={backgroundColor} />
+            </Link>
+          )
+        })}
       </section>
     </section>
   )
